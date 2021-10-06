d5b5e1ba56e44ac6c52d1cc7a414be3c15bea9cb
Arthur <arthurfreitascardoso@gmail.com>
2021-10-06 11:08:27 +0200

    follow heroku tutorial

--- comment/router.js
+++ comment/router.js
@@ -41,4 +41,17 @@ router.delete("/comment/:id", (request, response, next) =>
     .catch(next)
 );
 
+router.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
 module.exports = router;
