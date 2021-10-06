c47dceec5b5c10e33e7bd3d7241371e6acc5378c
Arthur <arthurfreitascardoso@gmail.com>
2021-10-07 00:46:44 +0200

    second

--- comment/router.js
+++ comment/router.js
@@ -62,7 +62,8 @@ router.get('/db', async (req, res) => {
 })
 router.post('/db', async (req, res) => {
   try {
-    const { author, comment } = req.body
+    const  author  = req.body[author]
+    const  comment  = req.body[comment]
     const client = await pool.connect();
     const result = await client.query('INSERT INTO comment_table (author, comment) VALUES ($1, $2)', [author, comment]);
     const results = { 'results': (result) ? result.rows : null};
