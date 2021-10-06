041e03d724eee5abe01d148f4d1eb0b215de9b22
Arthur <arthurfreitascardoso@gmail.com>
2021-10-06 02:30:03 +0200

    db primary

--- db.js
+++ db.js
@@ -1,14 +1,6 @@
 const Sequelize = require("sequelize");
 const databaseUrl = process.env.DATABASE_URL || "postgres://postgres:postgres@localhost:5432/postgres";
-//const db = new Sequelize(databaseUrl);
-const db = {
-  "use_env_variable": "DATABASE_URL",
-  "dialectOptions": {
-    "ssl": {
-      "rejectUnauthorized": false
-    }
-  }
-}
+const db = new Sequelize(databaseUrl);
 
 db.sync()
   .then(() => console.log("Database connected!"))
