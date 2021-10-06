1a5f8b472d4d18b6baf273dae6115424261a6390
Arthur <arthurfreitascardoso@gmail.com>
2021-10-07 01:58:50 +0200

    refactor

--- comment/router.js
+++ comment/router.js
@@ -43,5 +43,17 @@ router.post('/comment', async (req, res) => {
     res.send("Error " + err);
   }
 })
+router.get('/delete', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('DELETE * FROM comment_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.send( results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 
 module.exports = router;
